e56d7b8f5db002c889e734bf1d519e6c6ecbd71f
Adam Fahey <afahey87@gmail.com>
2017-12-03 11:51:45 -0500

    Initial commit

---
+++ Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+
+namespace mozillaTest
+{
+    [TestFixture]
+    class mozillaTest
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://mozilla.org");
+        }
+
+        [TearDown]
+        public void teardown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+        
+        [Test]
+        public void checkPageIsUp ()
+        {
+            Assert.True(driver.Url.Contains("US"));
+        }
+
+        [Test]
+        public void internetHealthReport()
+        {
+            driver.FindElement(By.Id("nethealth")).Click();
+            System.Threading.Thread.Sleep(1000);
+            Assert.True(driver.Url.Contains("https://internethealthreport.org/"));
+        }
+
+    }
+    
+
+
+}
